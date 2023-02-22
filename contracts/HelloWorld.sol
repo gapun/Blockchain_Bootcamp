e77f6bb7faccf1bbe905d2d3d4c21d9fa978a42f
gapun <gapun@yahoo.com>
2023-02-22 15:20:10 +0800

    Change Version

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.1;
+pragma solidity ^0.8.2;
 contract HelloWorld {
     function displayHello() public pure returns (string memory) {
         return "Hello World...";
